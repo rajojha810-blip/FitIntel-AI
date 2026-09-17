0782ed8f862108f3a0c93b4139de6787c4556898
Raj Ojha <rajojha810@gmail.com>
2026-09-17 13:52:56 +0530

    Remove try except to see real error

--- 02_database/db_connection.py
+++ 02_database/db_connection.py
@@ -1,18 +1,15 @@
-import traceback
 import mysql.connector
 import streamlit as st
 
 def get_connection():
-    try:
+    
         connection = mysql.connector.connect(
             host=st.secrets["DB_HOST"],
             port=int(st.secrets["DB_PORT"]),
             user=st.secrets["DB_USER"],
             password=st.secrets["DB_PASSWORD"],
             database=st.secrets["DB_NAME"],
-            ssl={"ssl":{}}
+            
         )
         return connection
-    except Exception as e:
-        st.error(traceback.format_exc())
-        return None
+    
